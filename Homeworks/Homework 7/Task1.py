6526d746f430b315381f45988f4ad265b98e3eab
Денис Билобрам <denis.bilobram@mail.ru>
2020-04-10 09:12:55 +0800

    fix

--- Homeworks/Homework 7/Task1.py
+++ Homeworks/Homework 7/Task1.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 class Matrix():
     def __init__(self, unit_matrix):
         self.unit_matrix = unit_matrix.copy()
@@ -9,7 +10,7 @@ class Matrix():
     def __str__(self):
         return self.ready_matrix
     def __add__(self, other):
-        between = Matrix(other.unit_matrix) #здесь передаётся ссылка, из-за этого меняется матрица правого операнда, не знаю как это починить
+        between = Matrix(deepcopy(other.unit_matrix)) #здесь передаётся ссылка, из-за этого меняется матрица правого операнда, не знаю как это починить
         for element in range(len(other.unit_matrix)):
             for num in range(len(other.unit_matrix[element])):
                 between.unit_matrix[element][num] += self.unit_matrix[element][num]
@@ -21,7 +22,6 @@ class Matrix():
         return between.ready_matrix
 matrix = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 matrix_sec = Matrix([[10, 11, 12], [13, 14, 15], [16, 17, 18]])
-print(matrix)
-print(matrix_sec)
+print(matrix + matrix_sec)
 print(matrix + matrix_sec)
 print(matrix + matrix_sec)
